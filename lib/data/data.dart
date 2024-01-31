99177c007b9494b1a81062b586fdaa632a013ff7
Dev81101 <jakuposki01@gmail.com>
2024-01-31 23:32:17 +0100

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,9 @@
+import '../model/item.dart';
+
+List<Item> itemList = [
+  Item(id:1,name: 'SHIRT', price: 500, image: 'https://content.moss.co.uk/images/original/04df98b6-0a29-4388-99a4-ec44750764d6.jpg'),
+  Item(id:2,name: 'T-SHIRT', price: 350, image: 'https://www.mrporter.com/variants/images/3633577411310824/in/w560_q60.jpg'),
+  Item(id:3,name: 'COAT', price: 1000, image: 'https://godwincharli.com/cdn/shop/products/MW22L-7-22971_667x.jpg'),
+  Item(id:4,name: 'SUIT', price: 2999, image: 'https://www.ernest.ca/cdn/shop/files/S16-4301406-PL-2-C.jpg'),
+  Item(id:5,name: 'PANTS', price: 1000, image: 'https://bonobos-prod-s3.imgix.net/products/276888/original/DENIM_5-POCKET-JEAN_27719-BK746_1.jpg'),
+];
